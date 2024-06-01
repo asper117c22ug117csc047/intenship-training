0d3f8773c5d95f64e6578207c99e0f79343c4089
asper117c22ug117csc047 <144678268+asper117c22ug117csc047@users.noreply.github.com>
2024-06-01 20:15:23 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,8 @@
+a=int(input("Enter 1st no"))
+b=int(input("Enter 2nd no"))
+c=int(input("Enter 3rd no"))
+print("\n Addition",a+b+c)
+print("Substraction",a-b-c)
+print("Multiply",a*b*c)
+print("Division",a/b)
 
